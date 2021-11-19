34e85b00c70441276ef2ebd4a56a40e3935b5d3d
LloydTao <LL23@live.co.uk>
2021-11-19 18:07:01 +0000

    Create application shell

---
+++ frontend/frontend/layouts/default.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="min-h-screen bg-gray-50">
+    <div class="bg-blue-700">
+      <Navbar />
+      <Nuxt />
+      <Footer />
+    </div>
+  </div>
+</template>
+
+<style></style>
