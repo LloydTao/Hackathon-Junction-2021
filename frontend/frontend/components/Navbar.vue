9667022b9f07a02d944fd888b26f71bfd811f410
Sam Bebbington <sam.bebbington@jjcfintech.com>
2021-11-20 23:59:55 +0000

    Login

--- frontend/frontend/components/Navbar.vue
+++ frontend/frontend/components/Navbar.vue
@@ -13,8 +13,8 @@
       </ul>
       <ul class="flex items-center space-x-5">
         <li class="hidden sm:flex">
-          <a
-            href="/"
+          <nuxt-link
+            to="/"
             class="
               text-gray-50
               hover:text-gray-400
@@ -23,12 +23,12 @@
               py-1
               px-3
             "
-            >Home</a
+            >Home</nuxt-link
           >
         </li>
         <li class="hidden sm:flex">
-          <a
-            href="/users"
+          <nuxt-link
+            to="/users"
             class="
               text-gray-50
               hover:text-gray-400
@@ -37,12 +37,12 @@
               py-1
               px-3
             "
-            >Users</a
+            >Users</nuxt-link
           >
         </li>
         <li class="hidden sm:flex">
-          <a
-            href="/rooms"
+          <nuxt-link
+            to="/rooms"
             class="
               text-gray-50
               hover:text-gray-400
@@ -51,7 +51,7 @@
               py-1
               px-3
             "
-            >Rooms</a
+            >Rooms</nuxt-link
           >
         </li>
       </ul>
---
+++ frontend/frontend/plugins/axios.js
@@ -0,0 +1,14 @@
+export default function ({ $axios, redirect }) {
+  $axios.onRequest((config) => {
+    // Add Auth Token if saved in local storage
+    if (process.client) {
+      const user = localStorage.getItem('user')
+      if (user) {
+        const token = JSON.parse().token
+        if (token) {
+          config.headers.Authorization = `Token ${token}`
+        }
+      }
+    }
+  })
+}
