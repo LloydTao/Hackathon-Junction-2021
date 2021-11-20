0ce6a3b6b38259c458612a6d43ee5e0d53ce76bd
Sam Bebbington <sam.bebbington@jjcfintech.com>
2021-11-20 01:50:55 +0000

    Lint 😎

--- backend/backend/messaging/viewsets.py
+++ backend/backend/messaging/viewsets.py
@@ -2,7 +2,12 @@ from rest_framework import status, viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from messaging.models import Room, Message
-from messaging.serializers import RoomSerializer, RoomUsersSerializer, MessageSerializer, MessagePostSerializer
+from messaging.serializers import (
+    RoomSerializer,
+    RoomUsersSerializer,
+    MessageSerializer,
+    MessagePostSerializer,
+)
 from accounts.serializers import UserSerializer
 from accounts.models.user import User
 
@@ -13,7 +18,6 @@ class RoomViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         return Room.objects.all().filter(users=self.request.user)
-
 
     @action(detail=True, methods=["get", "post"])
     def messages(self, request, pk=None):
@@ -27,7 +31,9 @@ class RoomViewSet(viewsets.ModelViewSet):
                     content=serializer.validated_data["content"],
                     sender=request.user,
                 )
-                return Response(MessageSerializer(new_message, context={"request": request}).data)
+                return Response(
+                    MessageSerializer(new_message, context={"request": request}).data
+                )
             else:
                 return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         print(room.messages)
@@ -37,44 +43,56 @@ class RoomViewSet(viewsets.ModelViewSet):
             ).data
         )
 
-    @action(detail=True, methods=['get', 'post'])
+    @action(detail=True, methods=["get", "post"])
     def users(self, request, pk=None):
         room = self.get_object()
         if request.method == "POST":
             serializer = RoomUsersSerializer(data=request.data)
             if serializer.is_valid():
-                user_list = User.objects.all().filter(id__in=serializer.validated_data['user_ids']).values_list('id', flat=True)
+                user_list = (
+                    User.objects.all()
+                    .filter(id__in=serializer.validated_data["user_ids"])
+                    .values_list("id", flat=True)
+                )
                 new_users = room.users.add(*user_list)
-                return Response(UserSerializer(room.users, many=True, context={"request": request}).data)
+                return Response(
+                    UserSerializer(
+                        room.users, many=True, context={"request": request}
+                    ).data
+                )
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        return Response(UserSerializer(room.users, many=True, context={"request": request}).data)
-
+        return Response(
+            UserSerializer(room.users, many=True, context={"request": request}).data
+        )
 
 
 class MessageViewSet(viewsets.ModelViewSet):
     queryset = Message.objects.all()
     serializer_class = MessageSerializer
 
-    
     def get_queryset(self):
         return Message.objects.all().filter(room__users=self.request.user)
 
-    @action(detail=True, methods=['post'])
+    @action(detail=True, methods=["post"])
     def flag(self, request, pk=None):
         message = self.get_object()
         if message.sender == request.user:
-            return Response({'detail': 'You cannot flag your own messages'}, status=status.HTTP_400_BAD_REQUEST)
+            return Response(
+                {"detail": "You cannot flag your own messages"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
         message.flags += 1
         message.save()
         return Response(MessageSerializer(message, context={"request": request}).data)
 
-    @action(detail=True, methods=['post'])
+    @action(detail=True, methods=["post"])
     def like(self, request, pk=None):
         message = self.get_object()
         if message.sender == request.user:
-            return Response({'detail': 'You cannot like your own messages'}, status=status.HTTP_400_BAD_REQUEST)
+            return Response(
+                {"detail": "You cannot like your own messages"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
         message.likes += 1
         message.save()
         return Response(MessageSerializer(message, context={"request": request}).data)
-
-
