e86746d2654817d8bc23d4544e5fdebfbb0765ee
LloydTao <LL23@live.co.uk>
2021-11-20 02:42:29 +0000

    Make dashboard cards more modern

--- frontend/frontend/components/DashboardCards.vue
+++ frontend/frontend/components/DashboardCards.vue
@@ -1,60 +1,64 @@
 <template>
-  <div class="flex space-x-4 mt-5">
+  <div class="flex space-x-4 mt-8 mb-12">
     <div
       id="card"
-      class="
-        flex flex-1
-        items-center
-        bg-white
-        border
-        rounded
-        shadow-sm
-        px-5
-        py-3
-      "
+      class="flex flex-1 items-center bg-white rounded-lg shadow-lg px-8 py-5"
     >
-      <div class="bg-red-500 h-10 w-10 rounded-full mr-3"></div>
-      <div class="flex flex-col">
-        <h2 class="text-gray-900 font-semibold">Users</h2>
-        <p class="text-gray-900">23</p>
+      <div class="flex flex-col text-blue-500">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-8 w-8"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+        >
+          <path
+            d="M9 6a3 3 0 11-6 0 3 3 0 016 0zM17 6a3 3 0 11-6 0 3 3 0 016 0zM12.93 17c.046-.327.07-.66.07-1a6.97 6.97 0 00-1.5-4.33A5 5 0 0119 16v1h-6.07zM6 11a5 5 0 015 5v1H1v-1a5 5 0 015-5z"
+          />
+        </svg>
+        <h2 class="text-lg text-gray-600 font-medium mt-2">Users</h2>
+        <p class="text-xl text-gray-800 font-bold mt-1">23</p>
       </div>
     </div>
     <div
       id="card"
-      class="
-        flex flex-1
-        items-center
-        bg-white
-        border
-        rounded
-        shadow-sm
-        px-5
-        py-3
-      "
+      class="flex flex-1 items-center bg-white rounded-lg shadow-lg px-8 py-5"
     >
-      <div class="bg-green-500 h-10 w-10 rounded-full mr-3"></div>
-      <div class="flex flex-col">
-        <h2 class="text-gray-900 font-semibold">Rooms</h2>
-        <p class="text-gray-900">1</p>
+      <div class="flex flex-col text-yellow-500">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-8 w-8"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+        >
+          <path
+            d="M2 5a2 2 0 012-2h7a2 2 0 012 2v4a2 2 0 01-2 2H9l-3 3v-3H4a2 2 0 01-2-2V5z"
+          />
+          <path
+            d="M15 7v2a4 4 0 01-4 4H9.828l-1.766 1.767c.28.149.599.233.938.233h2l3 3v-3h2a2 2 0 002-2V9a2 2 0 00-2-2h-1z"
+          />
+        </svg>
+        <h2 class="text-lg text-gray-600 font-medium mt-2">Rooms</h2>
+        <p class="text-xl text-gray-800 font-bold mt-1">1</p>
       </div>
     </div>
     <div
       id="card"
-      class="
-        flex flex-1
-        items-center
-        bg-white
-        border
-        rounded
-        shadow-sm
-        px-5
-        py-3
-      "
+      class="flex flex-1 items-center bg-white rounded-lg shadow-lg px-8 py-5"
     >
-      <div class="bg-blue-500 h-10 w-10 rounded-full mr-3"></div>
-      <div class="flex flex-col">
-        <h2 class="text-gray-900 font-semibold">Messages</h2>
-        <p class="text-gray-900">562</p>
+      <div class="flex flex-col text-green-500">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-8 w-8"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+        >
+          <path
+            d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"
+          />
+          <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
+        </svg>
+        <h2 class="text-lg text-gray-600 font-medium mt-2">Messages</h2>
+        <p class="text-xl text-gray-800 font-bold mt-1">562</p>
       </div>
     </div>
   </div>
