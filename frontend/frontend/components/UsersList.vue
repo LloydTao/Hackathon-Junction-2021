82e0c830a1c0c80be7394bcc853467200e8c001d
LloydTao <LL23@live.co.uk>
2021-11-20 00:15:16 +0000

    Create users pages

---
+++ frontend/frontend/components/UsersList.vue
@@ -0,0 +1,132 @@
+<template>
+  <section class="container mx-auto">
+    <div class="w-full rounded-lg">
+      <div class="w-full overflow-x-auto">
+        <h1 class="text-xl text-gray-900 font-semibold mt-3">Users</h1>
+        <table class="w-full font-mono mt-5">
+          <thead>
+            <tr
+              class="
+                font-semibold
+                text-left text-gray-900
+                bg-gray-100
+                uppercase
+              "
+            >
+              <th class="px-4 py-3">User</th>
+              <th class="px-4 py-3">Created</th>
+              <th class="px-4 py-3">Rating</th>
+              <th class="px-4 py-3">Status</th>
+            </tr>
+          </thead>
+          <tbody class="bg-white">
+            <tr v-for="user in users" :key="user" class="text-gray-700">
+              <td class="px-4 py-3">
+                <div class="flex items-center text-sm">
+                  <div class="relative w-8 h-8 mr-3 rounded-full md:block">
+                    <img
+                      class="object-cover w-full h-full rounded-full"
+                      src="https://placekitten.com/160/160"
+                      alt=""
+                      loading="lazy"
+                    />
+                    <div
+                      class="absolute inset-0 rounded-full shadow-inner"
+                      aria-hidden="true"
+                    ></div>
+                  </div>
+                  <div>
+                    <NuxtLink
+                      class="font-semibold text-black hover:underline"
+                      :to="{ name: 'users-id', params: { id: user.id } }"
+                    >
+                      <span>
+                        {{ user.username }}
+                      </span>
+                    </NuxtLink>
+                    <p class="text-xs text-gray-600">{{ user.role }}</p>
+                  </div>
+                </div>
+              </td>
+              <td class="px-4 py-3 text-sm">{{ user.created }}</td>
+              <td class="px-4 py-3 text-sm">{{ user.rating }}</td>
+              <td class="px-4 py-3 text-xs">
+                <span
+                  v-if="user.status"
+                  class="
+                    px-2
+                    py-1
+                    font-semibold
+                    leading-tight
+                    text-green-700
+                    bg-green-100
+                    rounded-sm
+                  "
+                >
+                  Online
+                </span>
+                <span
+                  v-else
+                  class="
+                    px-2
+                    py-1
+                    font-semibold
+                    leading-tight
+                    text-gray-700
+                    bg-gray-100
+                    rounded-sm
+                  "
+                >
+                  Offline
+                </span>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      users: [
+        {
+          id: 1,
+          username: 'lloydtao',
+          role: 'Player',
+          created: '2021-11-16',
+          rating: 1000,
+          status: 1,
+        },
+        {
+          id: 2,
+          username: 'gtbebbo',
+          role: 'Player',
+          created: '2021-11-17',
+          rating: 943,
+          status: 0,
+        },
+        {
+          id: 3,
+          username: 'amystell',
+          role: 'Player',
+          created: '2021-11-18',
+          rating: 1057,
+          status: 0,
+        },
+        {
+          id: 4,
+          username: 'keith-harrison',
+          role: 'Player',
+          created: '2021-11-19',
+          rating: 1336,
+          status: 1,
+        },
+      ],
+    }
+  },
+}
+</script>
---
+++ frontend/frontend/pages/users/_id.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="bg-gray-50">
+    <div class="flex flex-col container mx-auto px-5 py-5">
+      <UsersDetail />
+      <div class="mt-8" />
+      <MessagesList />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      id: this.$route.params.id,
+    }
+  },
+}
+</script>
