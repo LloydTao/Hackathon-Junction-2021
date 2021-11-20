8ccf6048c4ca8ba332837ce67e129ee5e4247be9
LloydTao <LL23@live.co.uk>
2021-11-20 20:43:24 +0000

    Modernise dashboard chat room

--- frontend/frontend/components/DashboardChat.vue
+++ frontend/frontend/components/DashboardChat.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
-    <table class="w-full font-mono border rounded shadow-sm mt-5">
+    <table class="w-full border rounded shadow-md mt-5">
       <thead>
-        <tr class="font-semibold text-left text-gray-900 bg-gray-100 uppercase">
+        <tr class="text-left text-gray-900 bg-gray-100">
           <th class="px-4 py-3">User</th>
           <th class="px-4 py-3">Content</th>
           <th class="px-4 py-3">Actions</th>
@@ -43,47 +43,49 @@
               </div>
             </td>
             <td class="px-4 py-3 text-sm">{{ message.content }}</td>
-            <td class="flex items-center space-x-3">
-              <div class="text-gray-50 bg-blue-500 h-5 w-5 rounded-full">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  class="p-1 h-5 w-5"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                >
-                  <path
-                    d="M2 10.5a1.5 1.5 0 113 0v6a1.5 1.5 0 01-3 0v-6zM6 10.333v5.43a2 2 0 001.106 1.79l.05.025A4 4 0 008.943 18h5.416a2 2 0 001.962-1.608l1.2-6A2 2 0 0015.56 8H12V4a2 2 0 00-2-2 1 1 0 00-1 1v.667a4 4 0 01-.8 2.4L6.8 7.933a4 4 0 00-.8 2.4z"
-                  />
-                </svg>
-              </div>
-              <div class="text-gray-50 bg-red-500 h-5 w-5 rounded-full">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  class="p-1 h-5 w-5"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                >
-                  <path
-                    fill-rule="evenodd"
-                    d="M3 6a3 3 0 013-3h10a1 1 0 01.8 1.6L14.25 8l2.55 3.4A1 1 0 0116 13H6a1 1 0 00-1 1v3a1 1 0 11-2 0V6z"
-                    clip-rule="evenodd"
-                  />
-                </svg>
+            <td class="px-4 py-3">
+              <div class="flex items-center space-x-3">
+                <div class="text-gray-50 bg-blue-500 h-5 w-5 rounded-full">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="p-1 h-5 w-5"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                  >
+                    <path
+                      d="M2 10.5a1.5 1.5 0 113 0v6a1.5 1.5 0 01-3 0v-6zM6 10.333v5.43a2 2 0 001.106 1.79l.05.025A4 4 0 008.943 18h5.416a2 2 0 001.962-1.608l1.2-6A2 2 0 0015.56 8H12V4a2 2 0 00-2-2 1 1 0 00-1 1v.667a4 4 0 01-.8 2.4L6.8 7.933a4 4 0 00-.8 2.4z"
+                    />
+                  </svg>
+                </div>
+                <div class="text-gray-50 bg-red-500 h-5 w-5 rounded-full">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="p-1 h-5 w-5"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M3 6a3 3 0 013-3h10a1 1 0 01.8 1.6L14.25 8l2.55 3.4A1 1 0 0116 13H6a1 1 0 00-1 1v3a1 1 0 11-2 0V6z"
+                      clip-rule="evenodd"
+                    />
+                  </svg>
+                </div>
               </div>
             </td>
           </template>
         </tr>
       </tbody>
     </table>
-    <form class="flex space-x-5 py-3">
+    <form class="flex space-x-5 py-3 mt-1">
       <input
         id="message"
         class="
           flex flex-1
-          shadow
           appearance-none
           border
           rounded
+          shadow-md
           w-full
           py-2
           px-3
@@ -103,6 +105,7 @@
           py-2
           px-4
           rounded
+          shadow-md
           focus:outline-none focus:shadow-outline
         "
         type="button"
