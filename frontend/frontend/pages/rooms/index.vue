4a20ae3a31d253e54b09c4c1cee613a379bafbf6
LloydTao <LL23@live.co.uk>
2021-11-20 00:15:25 +0000

    Create rooms pages

---
+++ frontend/frontend/pages/rooms/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="bg-gray-50">
+    <div class="flex container mx-auto px-5 py-5">
+      <RoomsList />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
