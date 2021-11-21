04e2be97ac0ba687dede41c3a2adb09f84416541
LloydTao <LL23@live.co.uk>
2021-11-21 02:48:11 +0000

    Fix missing token in auth middleware

--- frontend/frontend/plugins/axios.js
+++ frontend/frontend/plugins/axios.js
@@ -4,7 +4,7 @@ export default function ({ $axios, redirect }) {
     if (process.client) {
       const user = localStorage.getItem('user')
       if (user) {
-        const token = JSON.parse().token
+        const token = JSON.parse(user).token
         if (token) {
           config.headers.Authorization = `Token ${token}`
         }
